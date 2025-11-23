ef135dfa1796e2bf16b84207e5732fda2b844ab8
YenHsinWu <rex19960408@gmail.com>
2025-11-24 04:08:45 +0800

    direct link to corresponding chatroom by slug

--- chatapp/views.py
+++ chatapp/views.py
@@ -8,3 +8,10 @@ def index(request):
         'chatrooms': chatrooms
     }
     return render(request, "index.html", context)
+
+def chatroom(request, slug):
+    chatroom = ChatRoom.objects.get(slug=slug)
+    context = {
+        'chatroom': chatroom
+    }
+    return render(request, "chatapp/room.html", context)
