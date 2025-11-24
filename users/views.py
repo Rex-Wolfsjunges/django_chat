f31314bc1108163a8219f38aa3b5d680ccff63ad
YenHsinWu <rex19960408@gmail.com>
2025-11-24 23:46:35 +0800

    auth functionalities

--- users/views.py
+++ users/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    return render(request, "users/register.html")
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+
+            return redirect("index")
+    context = {
+        "form": form
+    }
+    return render(request, "users/register.html", context)
+
+def logout_view(request):
+    logout(request)
+
+    return redirect("index")
